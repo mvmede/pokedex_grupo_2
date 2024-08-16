bf9efb9f60b28437e38cc46f6e7dc1c0522caeae
vnnbrt <vinniebritos@gmail.com>
2024-08-15 22:39:38 -0300

    adiciona função de pesquisa com interação do usuário. - criação da função principal 'main' com prompts e alerts para interação com o usuário. - melhoria das mensagens dos prompts e alerts e na apresentação dos resultados.

--- script/index.js
+++ script/index.js
@@ -3,29 +3,51 @@ const pokedex = [
     {name: "Charmander", category: "Lizard", abilities: "Blaze", type: "Fogo"},
     {name: "Squirtle", category: "Tiny Turtle", abilities: "Torrent", type: "Água"},
     {name: "Vulpix", category: "Fox", abilities: "Flash Fire", type: "Fogo"},
-    {name: "", category: "", abilities: "", type: ""},
-    {name: "", category: "", abilities: "", type: ""},
-    {name: "", category: "", abilities: "", type: ""},
-    {name: "", category: "", abilities: "", type: ""},
+    {name: "Pidgey", category: "Tiny Bird", abilities: "Keen Eye / Tangled Feet", type: "Normal / Voador"},
+    {name: "Ekans", category: "Snake", abilities: "Shed Skin / Intimidate", type: "Veneno"},
+    {name: "Pikachu", category: "Mouse", abilities: "Static", type: "Elétrico"},
+    {name: "Psyduck", category: "Duck", abilities: "Damp / Cloud Nine", type: "Água"},
 ]
 
 function searchPokemonByName(name){
     const pokemon = pokedex.find(pokemon => pokemon.name.toLowerCase() === name.toLowerCase());
     if (pokemon) {
-        return pokemon;
+        return `Detalhes do Pokémon encontrado:\n\nNome: ${pokemon.name}\nCategoria: ${pokemon.category}\nHabilidades: ${pokemon.abilities}\nTipo: ${pokemon.type}`;
     } else {
-        return "Pokémon não encontrado."
+        return "Pokémon não encontrado. Verifique o nome e tente novamente."
     }
 }
 
 function searchPokemonByType(type){
     const result = pokedex.filter(pokemon => pokemon.type.toLowerCase().includes(type.toLowerCase()));
     if (result.length === 0){
-        return "Nenhum pokémon encontrado para o tipo especificado ou tipo inválido."
+        return "Nenhum Pokémon encontrado para o tipo especificado. Verifique o tipo e tente novamente."
     } else {
-        return result;
+        return `Resultado da pesquisa para o tipo '${type}':\n\n` + 
+            result.map(pokemon =>
+                `Nome: ${pokemon.name}\nCategoria: ${pokemon.category}\nHabilidades: ${pokemon.abilities}\nTipo: ${pokemon.type}`
+            ).join('\n-------------------------\n');
     }
 }
 
-console.log(searchPokemonByType("Choque"));
-console.log(searchPokemonByType("Fogo"));
+function main() {
+    while(true) {
+        let search = prompt("Bem-vindo ao Pokédex!\n\nEscolha uma das opções abaixo para pesquisar um Pokémon:\n1. Buscar por nome\n2. Buscar por tipo\n\nDigite 'sair' para encerrar.");
+
+        if (search === '1'){
+            let name = prompt("Digite o nome do Pokémon que deseja buscar:");
+            alert(searchPokemonByName(name));
+        } else if (search === '2'){
+            let type = prompt("Digite o tipo do Pókemon (por exemplo: 'Fogo', 'Água', etc.):");
+            alert(searchPokemonByType(type));
+        } else if (search.toLowerCase() === 'sair'){
+            alert("Obrigado por usar a Pokédex! Até a próxima!");
+            break;
+        } else {
+            alert("Opção inválida. Por favor, digite '1' para buscar por nome, '2' para buscar por tipo ou 'sair' para encerrar.")
+        }
+    }
+
+}
+
+main ();
