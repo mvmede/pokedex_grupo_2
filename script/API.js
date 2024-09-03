2c687b68b99c5f6f9679abe01d92f4c71429a426
Gustavo <gustavomamede99@gmail.com>
2024-09-03 20:09:46 -0300

    ajuste de imagens

---
+++ script/API.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const botao = document.getElementById("search")
+    botao.addEventListener("click", () => {
+        alert("teste")
+    })
+
+})
+
+//8dfce75e-09d7-4350-92f1-99bdabe56493
