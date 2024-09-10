336e391227186081605d9e1e150230d25bde7eb1
Gustavo <gustavomamede99@gmail.com>
2024-09-10 00:13:01 -0300

    Finalizando a API

--- script/API.js
+++ script/API.js
@@ -1,10 +1,76 @@
+
+function getCards(searchValue) {
+
+    if (searchValue === undefined || searchValue === null) {
+
+        searchValue = document.getElementById("searchInput").value
+
+    }
+
+    const searchValueEncoded = encodeURIComponent("name:" + searchValue)
+
+    fetch("https://api.pokemontcg.io/v2/cards?q=" + searchValueEncoded, {
+        method: "GET",
+        headers: {
+            "X-Api-Key": "8dfce75e-09d7-4350-92f1-99bdabe56493"
+        }
+
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error('Erro na requisição: ' + response.statusText);
+        }
+        return response.json();
+    }).then((data) => {
+        const cards = data.data
+        document.getElementById("searchResults").innerHTML += `
+            <strong>Resultados de pesquisa:</strong> ${cards.length} Cartas
+        `
+        cards.forEach(element => {
+
+            let attacks = ""
+            element.attacks.forEach(attack => {
+                attacks += `
+                    <p><b>${attack.name}:</b> ${attack.text}</p>
+                `
+            });
+
+            document.getElementById("cardsResults").innerHTML += `
+            
+            <div class="card-container">
+                <div class="card-image">
+                    <img src="${element.images.small}" alt="Imagem da Carta">
+                </div>
+                <div class="card-details">
+                    <h2>${element.name}</h2>
+                    <p><strong>Tipo:</strong> ${element.types[0]}</p>
+                    ${attacks}
+                </div>
+                <div class="card-action">
+                    <a href="#" class="btn">VER CARTA</a>
+                </div>
+            </div>
+
+            `
+        });
+    })
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
+    const urlParameter = new URLSearchParams(window.location.search)
+    if (urlParameter.get("q") !== undefined && urlParameter.get("q") !== null) {
+
+        getCards(urlParameter.get("q"))
+
+    }
+
+    const form = document.getElementById("searchForm")
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        getCards()
+    })
     const botao = document.getElementById("search")
     botao.addEventListener("click", () => {
-        alert("teste")
+        getCards()
     })
-
 })
-
-//8dfce75e-09d7-4350-92f1-99bdabe56493
--- script/index.js
+++ script/index.js
@@ -1,56 +1,21 @@
-<<<<<<< HEAD
-/*const pokedex = [
-=======
-/* const pokedex = [
->>>>>>> 3e1b88b70cfda495e3f02229d580e3c1684f5e8f
-    {name: "Bulbasaur", category: "Seed", abilities: "Overgrow", type: "Grama / Veneno"},
-    {name: "Charmander", category: "Lizard", abilities: "Blaze", type: "Fogo"},
-    {name: "Squirtle", category: "Tiny Turtle", abilities: "Torrent", type: "Água"},
-    {name: "Vulpix", category: "Fox", abilities: "Flash Fire", type: "Fogo"},
-    {name: "Pidgey", category: "Tiny Bird", abilities: "Keen Eye / Tangled Feet", type: "Normal / Voador"},
-    {name: "Ekans", category: "Snake", abilities: "Shed Skin / Intimidate", type: "Veneno"},
-    {name: "Pikachu", category: "Mouse", abilities: "Static", type: "Elétrico"},
-    {name: "Psyduck", category: "Duck", abilities: "Damp / Cloud Nine", type: "Água"},
-]
+function goToSearch() {
 
-function searchPokemonByName(name) {
-    const pokemon = pokedex.find(pokemon => pokemon.name.toLowerCase() === name.toLowerCase());
-    return pokemon
-        ? `Detalhes do Pokémon encontrado:\n\nNome: ${pokemon.name}\nCategoria: ${pokemon.category}\nHabilidades: ${pokemon.abilities}\nTipo: ${pokemon.type}`
-        : "Pokémon não encontrado. Verifique o nome e tente novamente.";
+    const searchValue = document.getElementById("searchInput").value
+    const searchValueEncoded = encodeURIComponent(searchValue)
+    window.location.href = "/html/search.html?q=" + searchValueEncoded
+
 }
 
-function searchPokemonByType(type) {
-    const result = pokedex.filter(pokemon => pokemon.type.toLowerCase().includes(type.toLowerCase()));
-    return result.length === 0
-        ? "Nenhum Pokémon encontrado para o tipo especificado. Verifique o tipo e tente novamente."
-        : `Resultado da pesquisa para o tipo '${type}':\n\n` +
-        result.map(pokemon =>
-            `Nome: ${pokemon.name}\nCategoria: ${pokemon.category}\nHabilidades: ${pokemon.abilities}\nTipo: ${pokemon.type}`
-        ).join('\n-------------------------\n');
-}
 
-function main() {
-    while(true) {
-        let search = prompt("Bem-vindo ao Pokédex!\n\nEscolha uma das opções abaixo para pesquisar um Pokémon:\n\n1. Buscar por nome\n2. Buscar por tipo\n3. Encerrar");
+document.addEventListener("DOMContentLoaded", () => {
 
-        switch (search) {
-            case '1':
-                let name = prompt("Digite o nome do Pokémon que deseja buscar:");
-                alert(searchPokemonByName(name));
-                break;
-            case '2':
-                let type = prompt("Digite o tipo do Pokémon (por exemplo: 'Fogo', 'Água', etc.):");
-                alert(searchPokemonByType(type));
-                break;
-            case '3':
-                alert("Obrigado por usar a Pokédex! Até a próxima!");
-                return;
-            default:
-                alert("Opção inválida. Por favor, digite '1' para buscar por nome, '2' para buscar por tipo ou '3' para encerrar.");
-                break;
-        }
-    }
-}
-
-main(); */
+    const form = document.getElementById("searchForm")
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        goToSearch()
+    })
+    const botao = document.getElementById("search")
+    botao.addEventListener("click", () => {
+        goToSearch()
+    })
+})
